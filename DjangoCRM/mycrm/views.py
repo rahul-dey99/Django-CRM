ccfdfa1c5db08ccfc91692014fc25e518250ddad
rahul-dey99 <rahul.dey.741787@gmail.com>
2024-02-20 15:12:45 +0530

    second commit

--- DjangoCRM/mycrm/views.py
+++ DjangoCRM/mycrm/views.py
@@ -1,4 +1,20 @@
-from django.shortcuts import render, render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 def home(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user.is_authenticated:
+            login(request, user)
+            messages.success(request, "Logged in succesfully!")
+            return redirect('home')
+        else:
+            messages.success(request, "Invalid credentials.")
+            return redirect('home')
+    else:
+        return render(request, 'home.html', {})
+        
     return render(request, 'home.html', {})
